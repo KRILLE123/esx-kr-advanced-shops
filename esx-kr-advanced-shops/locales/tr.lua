76c942e4441197299543ed72b5f5933fb888dd88
Doğacan Koç <42408796+Dogacanacc@users.noreply.github.com>
2019-03-19 10:43:59 +0300

    Create tr.lua
    Turkish language

---
+++ esx-kr-advanced-shops/locales/tr.lua
@@ -0,0 +1,10 @@
+Locales['tr'] = {
+  ['press_to_open'] = 'Menüyü açmak için ~INPUT_CONTEXT~ tuşuna basın.',
+	['press_to_rob'] = '~w~Mağazayı ~r~soymak ~w~için ~INPUT_CONTEXT~ tuşuna basın.',
+	['press_to_open_center'] = 'Market menüsünü açmak için ~INPUT_CONTEXT~ tuşuna basın.',
+	['name_shop'] = 'Marketine ne isim vermek istersin?',
+	['buy_shop'] = 'Marketi satın al ',
+	['robbery_cancel'] = '~r~Soygun iptal edildi! (Çok fazla uzaklaştın)',
+	['set_price'] = '~g~Satmak istediğiniz ne fiyatı ayarlayın.',
+	['how_much'] = '~g~Ne kadar satmak istersiniz?',
+}
